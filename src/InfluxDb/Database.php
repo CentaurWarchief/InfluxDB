06932a770c5d1c8fad8008244860b99943f5f9fe
Andrey <andreykvital@gmail.com>
2015-07-22 10:12:52 -0300

    Update Database

--- src/InfluxDb/Database.php
+++ src/InfluxDb/Database.php
@@ -4,4 +4,33 @@ namespace InfluxDb;
 
 class Database
 {
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var DriverInterface
+     */
+    private $driver;
+
+    /**
+     * @param string          $name
+     * @param DriverInterface $driver
+     */
+    public function __construct($name, DriverInterface $driver)
+    {
+        $this->name   = $name;
+        $this->driver = $driver;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    // (...)
 }
